9dc4a75c9324f21757e22a312cabcc7c07a85a13
jindalshiva <jindalshivani11@gmail.com>
2019-08-31 18:21:33 +0530

    connected with firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+  apiKey: " AIzaSyDHMTseKhFPqjcC1zy6hI3jHrBrHRMea1s ",
+  authDomain: "celestinidataset.firebaseapp.com",
+  databaseURL: "https://celestinidataset.firebaseio.com/",
+  projectId: "celestinidataset",
+  storageBucket: "celestinidataset.appspot.com",
+  messagingSenderId: "412906791597",
+  appID: "1:412906791597:web:ea23bf61b0d1bfd9",
+};
+firebase.initializeApp(config);
